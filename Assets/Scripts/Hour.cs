eb9116fb8cf892f4d7c716f77da31f9942afbb6c
BurAndBY <bur.and.by@gmail.com>
2021-12-06 10:30:12 +0300

    Clock for PSVita! For free!
    Clock for PSVita! For free!

---
+++ Assets/Scripts/Hour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class Hour : MonoBehaviour {
+
+	public Text HourText;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DateTime time = DateTime.Now;
+		string hour = LeadingZero(time.Hour);
+		HourText.text = hour;
+	}
+	string LeadingZero(int n)
+	{
+		return n.ToString().PadLeft(2, '0');
+	}
+}
---
+++ Assets/Scripts/Mins.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class Mins : MonoBehaviour
+{
+
+	public Text MinsText;
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		DateTime time = DateTime.Now;
+		string mins = LeadingZero(time.Minute);
+		MinsText.text = mins;
+	}
+	string LeadingZero(int n)
+	{
+		return n.ToString().PadLeft(2, '0');
+	}
+}
